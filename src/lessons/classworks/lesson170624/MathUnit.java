a99dfe32e12df74bc0ef586f5c3ea8f1a36f21db
Rustam Gurd <rustamkurdov@mail.ru>
2017-06-26 21:40:58 +0300

    Commit 1:
    Homework #1

---
+++ src/lessons/classworks/lesson170624/MathUnit.java
@@ -0,0 +1,14 @@
+package lessons.classworks.lesson170624;
+
+public class MathUnit
+{
+    public static int maxValue(int i, int j)
+    {
+        return i > j ? i : j;
+    }
+
+    public static int minValue(int i, int j)
+    {
+        return i < j ? i : j;
+    }
+}
---
+++ src/lessons/classworks/lesson170624/SimpleUnit.java
@@ -0,0 +1,46 @@
+package lessons.classworks.lesson170624;
+
+public class SimpleUnit
+{
+
+    public static boolean assertEquals(String actual, String expected)
+    {
+        boolean equals = actual.equals(expected);
+        if (!equals)
+        {
+            System.err.println("Test failed: expected " + expected + ", but actual " + actual);
+        }
+        return equals;
+    }
+
+    public static boolean assertNotEquals(String actual, String expected)
+    {
+        boolean notEquals = !actual.equals(expected);
+        if (!notEquals)
+        {
+            System.err.println("Test failed: expected NOT " + expected + ", but actual " + actual);
+        }
+        return notEquals;
+    }
+
+    public static boolean assertEquals(int actual, int expected)
+    {
+        boolean equals = actual == expected;
+        if (!equals)
+        {
+            System.err.println("Test failed: expected " + expected + ", but actual " + actual);
+        }
+        return equals;
+    }
+
+    public static boolean assertNotEquals(int actual, int expected)
+    {
+        boolean notEquals = actual != expected;
+        if (!notEquals)
+        {
+            System.err.println("Test failed: expected NOT " + expected + ", but actual " + actual);
+        }
+        return notEquals;
+    }
+
+}
