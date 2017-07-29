caba865f7fd8ccf676959941cf5c5c8a47375b59
Rustam Gurd <rustamkurdov@mail.ru>
2017-07-30 00:48:13 +0300

    MyStack (TDD)

--- src/lessons/homeworks/tasks170712/MyStack.java
+++ src/lessons/homeworks/tasks170712/MyStack.java
@@ -2,9 +2,28 @@ package lessons.homeworks.tasks170712;
 
 public class MyStack <E>
 {
+    private static final int DEFAULT_MAX_SIZE = 3;
+    private E[] elements;
+    private int size;
 
-    public boolean push(E one)
+    public MyStack()
     {
+        this(DEFAULT_MAX_SIZE);
+    }
+
+    public MyStack(int maxSize)
+    {
+        elements = (E[]) new Object[maxSize];
+    }
+
+    public boolean push(E element)
+    {
+        if (size >= elements.length)
+        {
+            return false; //stack overflow
+        }
+
+        elements[size++] = element;
         return true;
     }
 }
