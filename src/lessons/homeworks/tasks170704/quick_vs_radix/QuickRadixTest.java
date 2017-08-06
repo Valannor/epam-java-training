2661eee573fcc53812fa43c39b6c96646043462f
Rustam Gurd <rustamkurdov@mail.ru>
2017-08-07 00:58:19 +0300

    Radix sort

--- src/lessons/homeworks/tasks170704/quick_vs_radix/QuickRadixTest.java
+++ src/lessons/homeworks/tasks170704/quick_vs_radix/QuickRadixTest.java
@@ -9,18 +9,24 @@ public class QuickRadixTest
 {
     public static void main(String[] args)
     {
-        int[] array1 = NumberSetGenerator.generate(10_000_000);
-        int[] array2 = Arrays.copyOf(array1, 10_000_000);
+        int[] array1 = NumberSetGenerator.generate(1_000_000);
+        int[] array2 = Arrays.copyOf(array1, 1_000_000);
+        int[] array3 = Arrays.copyOf(array1, 1_000_000);
 
         Timer.startTimer();
         Arrays.sort(array1);
         System.out.println(Timer.stopTimer("Quick sort"));
 
         Timer.startTimer();
-        array2 = RadixSort.sort(array2);
-        System.out.println(Timer.stopTimer("Radix sort"));
+        array2 = RadixSort.sortWithRecursion(array2);
+        System.out.println(Timer.stopTimer("Radix sortWithRecursion"));
+
+        Timer.startTimer();
+        array3 = RadixSort.sortInLoop(array3);
+        System.out.println(Timer.stopTimer("Radix sortInLoop"));
 
         System.out.println(Arrays.equals(array1, array2));
+        System.out.println(Arrays.equals(array2, array3));
 
         //It works, but slower than regular quick sort.
     }
--- src/lessons/homeworks/tasks170704/quick_vs_radix/RadixSort.java
+++ src/lessons/homeworks/tasks170704/quick_vs_radix/RadixSort.java
@@ -4,9 +4,18 @@ import java.util.Arrays;
 
 public class RadixSort
 {
+    private static int getRadixElement(int number, int radix)
+    {
+        return (number / radix) % 10;
+    }
+
+
+    /**
+     * Implemented with recursion - bad speed
+     */
     private static int radix = 1;
 
-    public static int[] sort(int[] array)
+    public static int[] sortWithRecursion(int[] array)
     {
         int[] result = new int[array.length];
 
@@ -21,7 +30,7 @@ public class RadixSort
 
             for (int n : array)
             {
-                int element = getElement(n, radix);
+                int element = getRadixElement(n, radix);
                 if (sorted && element != 0)
                 {
                     sorted = false;
@@ -31,8 +40,7 @@ public class RadixSort
                 {
                     result[index] = n;
                     index++;
-                }
-                else
+                } else
                 {
                     if (temp.length - 1 < secondaryIndex)
                     {
@@ -50,22 +58,86 @@ public class RadixSort
         if (!sorted)
         {
             radix *= 10;
-            return sort(result);
+            return sortWithRecursion(result);
         }
 
         return result;
     }
 
-    private static int getElement(int number, int radix)
+
+    /**
+     * Sort in loop - bad speed
+     */
+    public static int[] oneIterationSort(int[] array, int radix)
     {
-        return (number / radix) % 10;
+        int[] result = new int[array.length];
+        int resultIndex = 0;
+        int unsortedBuffIndex = array.length;
+
+        for (int i = 0; i < 10; i++)
+        {
+            int[] unsortedBuffer = new int[unsortedBuffIndex]; // TODO: 06.08.2017 temporary
+            unsortedBuffIndex = 0;
+
+            for (int n : array)
+            {
+                int element = getRadixElement(n, radix);
+
+                if (element == i)
+                {
+                    result[resultIndex] = n;
+                    resultIndex++;
+                }
+                else
+                {
+                    if (unsortedBuffer.length - 1 < unsortedBuffIndex)
+                    {
+                        break;
+                    }
+                    unsortedBuffer[unsortedBuffIndex] = n;
+                    unsortedBuffIndex++;
+                }
+            }
+
+            array = unsortedBuffer;
+        }
+
+        return result;
     }
 
+    public static int[] sortInLoop(int[] array)
+    {
+        int[] result = array;
+
+        int reach = array[0];
+        for (int i : array)
+        {
+            reach = i > reach ? i : reach;
+        }
+
+        for (int i = 1; i <= reach; i *= 10)
+        {
+            result = oneIterationSort(result, i);
+        }
+
+        return result;
+    }
+
+
+    /**
+     * Implementation which works much better: https://gist.github.com/yeison/5606963
+     */
+
+
+    /**
+     * Local test
+     */
     public static void main(String[] args)
     {
         int[] array = {5, 50, 6, 4, 100, 2, 143, 5};
 
         // [2, 4, 5, 5, 6, 50, 100, 143]
-        System.out.println(Arrays.toString(sort(array)));
+//        System.out.println(Arrays.toString(sortWithRecursion(array)));
+        System.out.println(Arrays.toString(sortInLoop(array)));
     }
 }
