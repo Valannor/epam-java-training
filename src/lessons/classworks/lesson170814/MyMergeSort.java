a4d9ca2a34fb2b532369778404017d268645f322
Rustam Gurd <rustamkurdov@mail.ru>
2017-09-13 01:48:09 +0300

    170814 - MergeSort algorithm fix

--- src/lessons/classworks/lesson170814/MyMergeSort.java
+++ src/lessons/classworks/lesson170814/MyMergeSort.java
@@ -2,9 +2,9 @@ package lessons.classworks.lesson170814;
 
 import java.util.Arrays;
 
-public class Merge
+public class MyMergeSort
 {
-    public static void mergeArrays(int[] a, int[] aux, int lo, int hi, int mid)
+    private static void mergeArrays(Comparable[] a, Comparable[] aux, int lo, int hi, int mid)
     {
         for(int i = lo; i <= hi; i++)
             aux[i] = a[i];
@@ -16,14 +16,14 @@ public class Merge
                 a[k] = aux[j++];
             else if (j > hi)
                 a[k] = aux[i++];
-            else if (aux[j] < aux[i])
+            else if (aux[j].compareTo(aux[i]) < 0)
                 a[k] = aux[j++];
             else
                 a[k] = aux[i++];
         }
     }
 
-    public static void sortMerge(int[] a, int[] aux, int lo, int hi)
+    private static void sortMerge(Comparable[] a, Comparable[] aux, int lo, int hi)
     {
         if (hi <= lo) return;
 
@@ -33,15 +33,15 @@ public class Merge
         mergeArrays(a, aux, lo, hi, mid);
     }
 
-    public static void sort(int[] nums)
+    public static void sort(Comparable[] a)
     {
-        int[] aux = new int[nums.length];
-        sortMerge(nums, aux, 0, nums.length - 1);
+        Comparable[] aux = new Comparable[a.length];
+        sortMerge(a, aux, 0, a.length - 1);
     }
 
     public static void main(String[] args)
     {
-        int[] a = {10, 3, 13, 23, 25, 9, 94, 343, 1, 26, 1, 1, 3, 4, 6, 9, 2, 5, 7, 8, 10};
+        Integer[] a = {10, 3, 13, 23, 25, 9, 94, 343, 1, 26, 1, 1, 3, 4, 6, 9, 2, 5, 7, 8, 10};
         sort(a);
         System.out.println(Arrays.toString(a));
     }
