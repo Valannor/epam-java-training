978a93a381fb1a6ccf73d9875e882ffe05fd72a9
Rustam Gurd <rustamkurdov@mail.ru>
2017-09-12 21:55:52 +0300

    170814 - MergeSort algorithm practice 2

--- src/lessons/classworks/lesson170814/Merge.java
+++ src/lessons/classworks/lesson170814/Merge.java
@@ -4,32 +4,45 @@ import java.util.Arrays;
 
 public class Merge
 {
-    public static int[] mergeArrays(int[] a, int lo, int mid, int hi)
+    public static void mergeArrays(int[] a, int[] aux, int lo, int hi, int mid)
     {
-        int[] temp = new int[a.length];
-
-        for(int k = 0; k < hi; k++)
-            temp[k] = a[k];
+        for(int i = lo; i <= hi; i++)
+            aux[i] = a[i];
 
         int i = lo; int j = mid + 1;
-        for (int k = 0; k < hi; k++)
+        for (int k = lo; k <= hi; k++)
         {
             if (i > mid)
-                a[k] = temp[j++];
+                a[k] = aux[j++];
             else if (j > hi)
-                a[k] = temp[i++];
-            else if (temp[j] < temp[i])
-                a[k] = temp[j++];
+                a[k] = aux[i++];
+            else if (aux[j] < aux[i])
+                a[k] = aux[j++];
             else
-                a[k] = temp[i++];
+                a[k] = aux[i++];
         }
+    }
 
-        return a;
+    public static void sortMerge(int[] a, int[] aux, int lo, int hi)
+    {
+        if (hi <= lo) return;
+
+        int mid = lo + (hi - lo) / 2;
+        sortMerge(a, aux, lo, mid);
+        sortMerge(a, aux, mid + 1, hi);
+        mergeArrays(a, aux, lo, hi, mid);
+    }
+
+    public static void sort(int[] nums)
+    {
+        int[] aux = new int[nums.length];
+        sortMerge(nums, aux, 0, nums.length - 1);
     }
 
     public static void main(String[] args)
     {
-        int[] a = {1, 3, 4, 6, 9, 2, 5, 7, 8, 10};
-        System.out.println(Arrays.toString(mergeArrays(a,0, 4, 10)));
+        int[] a = {10, 3, 13, 23, 25, 9, 94, 343, 1, 26, 1, 1, 3, 4, 6, 9, 2, 5, 7, 8, 10};
+        sort(a);
+        System.out.println(Arrays.toString(a));
     }
 }
